5a50bae3e9b60722e1e5fd706bbc1ead5c128da2
Mehran <36104315+Mehran-at@users.noreply.github.com>
2019-03-04 04:59:06 -0500

    Update PrimeNumberApplication.java

--- src/main/java/ac/nacs/primenumberapplication/PrimeNumberApplication.java
+++ src/main/java/ac/nacs/primenumberapplication/PrimeNumberApplication.java
@@ -8,8 +8,6 @@ public class PrimeNumberApplication {
     public static void main(String[] args) {
         PrimeNumbersTillHundred primeNumbersTillHundred = new PrimeNumbersTillHundred();
         SpringApplication.run(PrimeNumberApplication.class, args);
-//        System.out.println("Prime numbers till hundred are: " + primeNumbersTillHundred.findPrimeNumbers());
-//        System.out.println(("Size of the prime numbers till hundred: " + primeNumbersTillHundred.findPrimeNumbers().size()));
         System.out.println("Prime numbers from 1 to " + primeNumbersTillHundred.maxCheck + " are: " + primeNumbersTillHundred.createPrimeString());
         System.out.println("The size of the list of prime numbers is " + (primeNumbersTillHundred.createPrimeString().size()));
     }
